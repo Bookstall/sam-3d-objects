7b89e7a712452b1dd219cc79107de70e241f1750
zhousijie <1269842362@qq.com>
2026-01-19 09:58:20 +0800

    demo 修改为本地的模型

--- demo.py
+++ demo.py
@@ -6,16 +6,17 @@ sys.path.append("notebook")
 from inference import Inference, load_image, load_single_mask
 
 # load model
-tag = "hf"
-config_path = f"checkpoints/{tag}/pipeline.yaml"
+config_path = "/data/models/LLM-models-file/sam-3d-objects/checkpoints/pipeline.yaml"
 inference = Inference(config_path, compile=False)
 
 # load image (RGBA only, mask is embedded in the alpha channel)
+# 加载图像（仅限 RGBA，Mask 在 Alpha 通道中进行 Embedding）
 image = load_image("notebook/images/shutterstock_stylish_kidsroom_1640806567/image.png")
 mask = load_single_mask("notebook/images/shutterstock_stylish_kidsroom_1640806567", index=14)
 
 # run model
 output = inference(image, mask, seed=42)
+print(f"keys of output: {output.keys()}")
 
 # export gaussian splat
 output["gs"].save_ply(f"splat.ply")
